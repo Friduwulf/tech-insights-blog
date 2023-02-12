6cea214795b628c8e0d5541a5915bcb9a326fd6a
Devin <devinkearney@gmail.com>
2023-02-12 17:02:48 -0400

    completed user routes for find users/user, create new user, login and logout

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -22,4 +22,80 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/:id', async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.params.id, {
+            include: [
+                {
+                    model: Post,
+                    attributes: ['id', 'title', 'date_created'],
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'date_created', 'post_id'],
+                    include: {
+                        model: Post,
+                        attributes: ['title']
+                    }
+                }
+            ],
+        });
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+        console.log(err);
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const userData = await User.create(req.body);
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+            res.status(200).json(userData); 
+        });
+    } catch (err) {
+     res.status(400).json(err);
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({
+            where: {
+                email: req.body.email
+            }
+        });
+        if (!userData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
+        const validPassword = await userData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+            res.json({ user: userData, message: 'You are now logged in!' });
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+
 module.exports = router;
