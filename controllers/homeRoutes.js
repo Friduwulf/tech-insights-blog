078a0ead936d55bd3d406b48c91784002e72d16b
Devin <devinkearney@gmail.com>
2023-02-22 23:12:15 -0400

    Homepage functional

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { Comment, Post, User } = require('../models');
 const withAuth = require('../utils/auth');
-
+var title = '';
 router.get('/', async (req, res) => {
     try {
         const postData = await Post.findAll({
@@ -10,17 +10,18 @@ router.get('/', async (req, res) => {
             model: User
             }],
         });
-        console.log(postData);
+        // console.log(postData);
         const plainPosts = postData.map((post) => post.get({ plain: true }));
         console.log(plainPosts);
+        title = 'Tech Insights Blog';
         res.render('homepage', {
-            plainPosts
+            plainPosts,
+            title
         });
     } catch (err) {
         res.status(500).json(err);
         console.log(err);
     }
 });
-
 
 module.exports = router;
